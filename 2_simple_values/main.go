6dceb53d7b5e19eefb66a621f235b09a8c589c74
bishnu-2003 <bishnugope4844@gmail.com>
2025-07-14 00:17:37 +0530

    changed to v2

---
+++ 2_simple_values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple value
+	fmt.Println(1)
+	// int
+	fmt.Println(1+1)
+	//string
+	fmt.Println("Hi Bro")
+	// Bool
+	fmt.Println(true)
+	//floats
+	fmt.Println(10.0)
+	
+}
