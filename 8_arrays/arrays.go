95d66574f8739e0e8ac5edb0494dfc70f2c5203c
bishnu-2003 <bishnugope4844@gmail.com>
2025-08-04 22:15:18 +0530

    array pat 3

--- 8_arrays/arrays.go
+++ 8_arrays/arrays.go
@@ -5,11 +5,17 @@ import "fmt"
 //number sequence of specific length
 
 func main(){
-	var num [4]int
+	//var num [4]int
 
 // array length
 	//fmt.Println(len(num))
 
-	num[0] = 1
-	fmt.Println(num[0])
+	//num[0] = 1
+	//fmt.Println(num[0])
+
+	//fmt.Println(num)
+
+/*var vals [4]bool
+vals[2] = true 
+fmt.Println(vals)*/
 }
