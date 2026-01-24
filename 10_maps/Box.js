0047a8fa0bb17cf8cf71de057d2da5cfceefef0a
bishnu-2003 <bishnugope4844@gmail.com>
2026-01-24 16:03:15 +0530

    js added

--- 10_maps/Box.js
+++ 10_maps/Box.js
@@ -1,5 +1,13 @@
-let fristName = "John";
+function greet(nameOfPerson) {
+
 console.log("hello" + fristName);
 console.log("namaste" + fristName);
 console.log("Kiwean" + fristName);;
 
+}
+
+let fristName = "harshit";
+let fristName2 = "mohit";
+
+greet(fristName);
+greet(fristName2);
